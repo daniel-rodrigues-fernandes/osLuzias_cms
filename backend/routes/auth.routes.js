122adeae3353eb721ead989f8422be8b091d2407
Daniel R. Fernanades <danielrodriguesfernandes10@gamil.com>
2026-04-06 14:37:34 -0300

    Rota de login adicionada

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -5,4 +5,7 @@ const authController = require('../controllers/auth.controller');
 // POST /auth/cadastro
 router.post('/auth/cadastro', authController.signupAutor);
 
+// POST /auth/login
+router.post('/auth/login', authController.loginAutor);
+
 module.exports = router;
