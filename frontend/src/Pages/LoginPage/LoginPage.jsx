290b0646ec4b9eff0ed48483151f1b75cd1920c9
Daniel R. Fernanades <danielrodriguesfernandes10@gamil.com>
2026-04-06 15:16:21 -0300

    Método POST adicionado ao login

--- frontend/src/Pages/LoginPage/LoginPage.jsx
+++ frontend/src/Pages/LoginPage/LoginPage.jsx
@@ -19,6 +19,7 @@ export default function LoginPage() {
                 headers: {
                     "Content-Type": "application/json"
                 },
+                method: "POST",
                 body: JSON.stringify({
                     email,
                     password
