d0a26f3ccace4fe18d7f0a0207e75ba077321684
Daniel R. Fernanades <danielrodriguesfernandes10@gamil.com>
2026-04-19 23:19:49 -0300

    NewArticle - Aplicação de estilo

--- frontend/src/Pages/NewArticle/NewArticle.jsx
+++ frontend/src/Pages/NewArticle/NewArticle.jsx
@@ -3,28 +3,28 @@ import estilo from './NewArticle.module.css'
 export default function NewArticle() {
     return (
         <main>
-            <header>
+            <header className={estilo['header']}>
                 <h1 className={estilo['header-title']}>Novo artigo</h1>
+                <div className={estilo['header-buttons']}>
+                    <button className={estilo['header-button']}>Salvar</button>
+                    <button className={estilo['header-button']}>Publicar</button>
+                    <button className={estilo['header-button']}>Descartar</button>
+                </div>
             </header>
-            <div>
-                <form>
-                    <div className={estilo['form-input']}>
+                <div className={estilo['article-form']}>
+                    <div className={estilo['form-group']}>
                         <label htmlFor="titulo">Título:</label>
                         <input type="text" id="titulo" name="titulo" />
                     </div>
-                    <div className={estilo['form-input']}>
+                    <div className={estilo['form-group']}>
                         <label htmlFor="resumo">Resumo:</label>
                         <input type="text" id="resumo" name="resumo" />
                     </div>
-                    <div className={estilo['form-input']}>
+                    <div className={estilo['form-group']}>
                         <label htmlFor="conteudo">Conteúdo:</label>
                         <textarea id="conteudo" name="conteudo"></textarea>
                     </div>
-                    <div>
-                        <button type="submit">Publicar</button>
-                    </div>
-                </form>
-            </div>
+                </div>
         </main>
     )
 }
